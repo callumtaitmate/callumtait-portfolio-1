fdd36b00fcf86427624a94d9c55170898bedafac
Callum <callumtaitmate@gmail.com>
2023-04-05 07:42:51 +0100

    progress bar

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,15 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import ProgressBar from "@badrap/bar-of-progress";
+const progress = new ProgressBar({
+  size: 4, 
+  color: "#FE595E",
+  className: "z-50",
+  delay: 100,
+});
+import Router from "next/router";
+Router.events.on("routeChangeStart", progress.start);
+Router.events.on("routeChangeComplete", progress.finish);
+Router.events.on("routeChangeError", progress.finish);
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
