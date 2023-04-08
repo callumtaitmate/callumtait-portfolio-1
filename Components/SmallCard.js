80bf0e56c821fdf7a0d641fdfbad8615d13fdb30
Callum <callumtaitmate@gmail.com>
2023-04-08 07:30:14 +0100

    smallCard location props

--- Components/SmallCard.js
+++ Components/SmallCard.js
@@ -5,7 +5,7 @@ function SmallCard({ img, distance, location }) {
   return (
     <div onClick={() =>
       router.push(
-        "/search?location=London&startDate=2023-04-19T23%3A00%3A00.000Z&endDate=2023-04-26T23%3A00%3A00.000Z&numberOfGuests=2"
+        `/search?location=${location}&startDate=2023-06-19T23%3A00%3A00.000Z&endDate=2023-06-26T23%3A00%3A00.000Z&numberOfGuests=2`
       )
     }>
       <div className="flex box item-center m-2 mt-5 space-x-4 rounded-xl cursor-pointer hover:bg-gray-100 hover:scale-95 transition transform duration-200 ease-out">
